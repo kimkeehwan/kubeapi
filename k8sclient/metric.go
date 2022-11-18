5f6bf0c84f8c190bde835bcea13db8686812d646
aiblabco <aiblab.co.kr@gmail.com>
2022-11-18 18:21:01 +0000

    add metrics

--- k8sclient/metric.go
+++ k8sclient/metric.go
@@ -1,19 +1,53 @@
 package k8sclient
 
 import (
+	"context"
 	"log"
 
-	metricsv "k8s.io/metrics/pkg/client/clientset/versioned"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	//metricsapi "k8s.io/metrics/pkg/apis/metrics"
+	metricsV1beta1api "k8s.io/metrics/pkg/apis/metrics/v1beta1"
+	metricsclientset "k8s.io/metrics/pkg/client/clientset/versioned"
 )
 
 type MetricImpl struct {
-	clients *metricsv.Clientset
+	clients *metricsclientset.Clientset
 }
 
 func NewK8sMetricClient(config *K8sClusterConfig) *MetricImpl {
-	client, err := metricsv.NewForConfig(config.config)
+	client, err := metricsclientset.NewForConfig(config.config)
 	if err != nil {
 		log.Fatalf("load fail metrics client %s", err.Error())
 	}
 	return &MetricImpl{clients: client}
 }
+
+func (s *MetricImpl) ListNode(ctx context.Context, selector string) (*metricsV1beta1api.NodeMetricsList, error) {
+	opts := metav1.ListOptions{}
+	if selector != "" {
+		opts.LabelSelector = selector
+	}
+	return s.clients.MetricsV1beta1().NodeMetricses().List(ctx, opts)
+}
+
+func (s *MetricImpl) GetNode(ctx context.Context, name string) (*metricsV1beta1api.NodeMetrics, error) {
+	opt := metav1.GetOptions{}
+
+	return s.clients.MetricsV1beta1().NodeMetricses().Get(ctx, name, opt)
+}
+
+func (s *MetricImpl) ListPod(ctx context.Context, namespace string, selector string) (*metricsV1beta1api.PodMetricsList, error) {
+	opts := metav1.ListOptions{}
+	if selector != "" {
+		opts.LabelSelector = selector
+	}
+	return s.clients.MetricsV1beta1().PodMetricses(namespace).List(ctx, opts)
+}
+
+func (s *MetricImpl) GetPod(ctx context.Context, namespace string, name string) (*metricsV1beta1api.PodMetrics, error) {
+	opt := metav1.GetOptions{}
+
+	return s.clients.MetricsV1beta1().PodMetricses(namespace).Get(ctx, name, opt)
+}
+
+//func (s *MetricImpl)
